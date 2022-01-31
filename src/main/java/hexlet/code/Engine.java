2fefd14c6de51fa10db77761563f921e20ff360b
Oxy <417ksena@gmail.com>
2022-02-01 00:06:26 +0200

    delete one cycle from Engine startGame

--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -10,18 +10,11 @@ public class Engine {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void startGame(Game game) {
-        GameKit[] gameKits = new GameKit[Game.NEEDED_SUCCESS_COUNT];
-        for (int i = 0; i < gameKits.length; i++) {
-            gameKits[i] = game.generateGameKit();
-        }
-        runGame(gameKits);
-    }
-
-    private static void runGame(GameKit[] gameKits) {
         final String name = Cli.greeting(scanner);
-        System.out.println(gameKits[0].getInfo());
+        GameKit gameKit = game.generateGameKit();
+        System.out.println(gameKit.getInfo());
         for (int answerCount = 0; answerCount < Game.NEEDED_SUCCESS_COUNT; answerCount++) {
-            GameKit gameKit = gameKits[answerCount];
+            gameKit = game.generateGameKit();
             makeQuestion(gameKit.getQuestion());
             String answer = getAnswer();
             if (!checkAnswer(gameKit.getExpectedAnswer(), answer)) {
